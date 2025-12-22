fa97710a3998ee86cd4c2ec1d6bd1f0945bf75a5
Your New Username <your-new-email@example.com>
2025-12-22 16:56:26 -0500

    Add Facebook embed to map section and update Instagram section with link card

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -33,6 +33,22 @@ const Map = () => {
               <p>Saturday: 9:00 AM - 6:00 PM</p>
               <p>Sunday: 10:00 AM - 5:00 PM</p>
             </div>
+            <div className="facebook-embed">
+              <h4>Follow Us on Facebook</h4>
+              <div className="fb-page-wrapper">
+                <iframe 
+                  src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fflowercitynails%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true" 
+                  width="100%" 
+                  height="500" 
+                  style={{ border: 'none', overflow: 'hidden' }} 
+                  scrolling="no" 
+                  frameBorder="0" 
+                  allowFullScreen={true}
+                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                  title="Flower City Nails Facebook Page"
+                ></iframe>
+              </div>
+            </div>
           </div>
         </div>
       </div>
